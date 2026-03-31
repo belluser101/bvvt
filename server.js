ce0fe29460709da59ebd5bd06044d497d5a93a43
belluser101 <ioihstueh@gmail.com>
2026-03-31 08:51:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
   'http://localhost:5173/',
   'https://gnaxd.vercel.app',
+  'https://obbell.vercel.app',
   'https://phenomenal-malasada-b21773.netlify.app'
    // add all your trusted frontends here
 ];
